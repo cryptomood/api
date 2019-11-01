d15d264c16eba7a16b343b41b6a2270c3d85726f
Ondrej Polakovič <ondrej@turing-technology.com>
2019-11-01 18:02:20 +0100

    *update* added token auth to go examples

--- go/MessagesProxy/SubscribeBaseTweet/client.go
+++ go/MessagesProxy/SubscribeBaseTweet/client.go
@@ -13,11 +13,27 @@ import (
 const CertFile = "./cert.pem"
 const Server = "SERVER"
 
+type tokenAuth struct {
+	token string
+}
+
+func (t tokenAuth) GetRequestMetadata(ctx context.Context, in ...string) (map[string]string, error) {
+	return map[string]string{
+		"authorization": "Bearer " + t.token,
+	}, nil
+}
+
+func (tokenAuth) RequireTransportSecurity() bool {
+	return true
+}
+
 func main() {
 	creds, err := credentials.NewClientTLSFromFile(CertFile, "")
 	if err != nil {
 		panic(err)
 	}
+	// in case you need token auth use command below
+	// conn, err := grpc.Dial(Server, grpc.WithTransportCredentials(creds), grpc.WithPerRPCCredentials(tokenAuth{"YOUR_TOKEN"}), grpc.WithTimeout(5*time.Second), grpc.WithBlock())
 	conn, err := grpc.Dial(Server, grpc.WithTransportCredentials(creds), grpc.WithTimeout(5*time.Second), grpc.WithBlock())
 	if err != nil {
 		panic(fmt.Sprintf("did not connect: %v", err))
